b1f4240158c1eee6ac74978ed48b2fcecef563bd
Ronitbanerjee572 <ronitbanerjee572@gmail.com>
2025-11-15 21:19:25 +0530

    Add index.php entry point

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../config/db.php';
+session_start();
+
+// If user is logged in, redirect to dashboard
+if (is_logged_in()) {
+    header('Location: dashboard.php');
+    exit;
+}
+
+// If user is not logged in, redirect to login
+header('Location: login.php');
+exit;
+?>
